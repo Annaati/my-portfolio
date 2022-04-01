89fb76c20111690264959968b3ae6925f610b8b8
Abdifatah Annaati <66587544+Annaati@users.noreply.github.com>
2022-04-01 23:59:07 +0300

    initial commit - webpack, folders, file, section, etc set up

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './styles/main.scss';
+import './scripts/main.js';
+
+
+import AnnaatiTech from'./assets/AnnaatiTech.png';
+import bgWave from './assets/bg-wave.svg'
+const brandimg = document.getElementById('brandimg');
+
+brandimg.src = AnnaatiTech;
+
