e406f95437acc05c3867c1bd3497d3b950a8bbab
Abdifatah Annaati <66587544+Annaati@users.noreply.github.com>
2022-04-08 01:04:10 +0300

    fixed mobile menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,29 @@
 //Add Sticky-Nav class on Scroll
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   var header = this.document.querySelector('header');
   if (window.scrollY > 0) {
     header.classList.add('sticky-nav');
   } else {
     header.classList.remove('sticky-nav');
   }
-}); 
+});
+
+//Active navLink
+var navLink = document.querySelectorAll('.navLink');
+document.querySelectorAll('.navLink').forEach(nl => nl.addEventListener('click', function () {
+  navLink.classList.toggle('active');
+}));
 
 //Toogle Mobile Menu
-window.addEventListener('click', function(e) {
-  var mm = document.getElementById("mobile-menu")
-  mm.classList.toggle('open');
+const navMenu = document.querySelector('.navMenu');
+const mobileMenu = document.querySelector('.mobileMenu');
+
+mobileMenu.addEventListener('click', () => {
+  navMenu.classList.toggle('open');
+  mobileMenu.classList.toggle('open');
 });
+
+document.querySelectorAll('.navLink').forEach(n => n.addEventListener('click', () =>{
+  navMenu.classList.remove('open');
+  mobileMenu.classList.remove('open');
+}))
